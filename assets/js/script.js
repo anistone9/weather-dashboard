9f37636eb73ab06f083b415fa99d6f8321d7ddc7
anistone9 <ani.stone.v@gmail.com>
2023-02-04 11:26:07 -0700

    added folder structure and skeleton code

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//Function for event handler (submit button) to retrieve user input (city data)
+
+//Function to fetch weather API using geographical coordinates
+
+//Function to display searched city weather conditions with city name, weather icon, temperature, humidity, and wind speed
+
+//Function to display 5-day forecast with the same data categories as above
+
+//Function to store searched city and weather results
+
+//Function to retrieve local storage data and display it when the button for that city is clicked
+
+//Function for event handler (button for each stored city)
+
+//Calls for the functions above
+
