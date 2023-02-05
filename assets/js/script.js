0ecedcf2bc9cdd0f9a36c02d05ec891485039a8c
anistone9 <ani.stone.v@gmail.com>
2023-02-05 16:03:43 -0700

    added local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var searchFormEl = document.getElementById('search-form');
 var cityInputEl = document.getElementById('city-name');
 var forecastContainerEl = document.getElementById('forecast-container');
 var forecastCardEl = document.getElementById('forecast-cards');
+var cityButtons = document.getElementById('city-buttons');
 var apiKey = '010f510da36ea60a956c7518452bbcb1';
 
 //Function for event handler (submit button) to retrieve user input (city data)
@@ -13,12 +14,13 @@ function formSubmitHandler(event) {
     var searchCity = cityInputEl.value.trim();
     console.log(searchCity);
 
-    //To DO: add if statement to check if input is null to alert user, or display results otherwise
+    //If statement to check if input is null to alert user, or display results otherwise
     if (searchCity) {
+        //Empty the container of any previous information when running searchApi function
+        forecastContainerEl.innerHTML = '';
+        forecastCardEl.innerHTML = '';
         cityApi(searchCity);
         //Empty search box after running the initial city search
-        //Empty the container of any previous information when running searchApri function
-        forecastContainerEl.textContent = '';
         cityInputEl.value = '';
     } else {
         alert('Please enter a city');
@@ -29,6 +31,10 @@ function formSubmitHandler(event) {
 function cityApi(city) {
     //Variable for geocode url plus the api key (concatenated)
     var geocodeUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=6&appid=' + apiKey;
+
+    //Clean up forecast container before fetching the API and running the function to display it
+    forecastContainerEl.innerHTML = '';
+    forecastCardEl.innerHTML = '';
 
     fetch(geocodeUrl)
         .then(function (response) {
@@ -58,8 +64,6 @@ function weatherApi(data) {
     var lon = data[0].lon;
     //Variable for weather api by city plus the api key (concatenated)
     var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&cnt=45&appid=' + apiKey;
-
-
 
     fetch(apiUrl)
         .then(function (response) {
@@ -126,6 +130,12 @@ function displayForecast(forecastData) {
 
 //Function to display 5-day forecast with the same data categories as above
 function displayFiveDays(forecastData) {
+
+    var subtitle = document.getElementById('subtitle');
+    var forecastSubtitle = document.createElement('h2');
+    forecastSubtitle.textContent = '5 Day Forecast:';
+    subtitle.appendChild(forecastSubtitle);
+
     for (var i = 0; i < 5; i++) {
         var titleEl = document.createElement('h2');
         var newDate = new Date(forecastData.list[i * 8].dt_txt);
@@ -156,18 +166,62 @@ function displayFiveDays(forecastData) {
         humidEl.innerHTML = 'Humidity: ' + humidityData + '%';
         forecastDayCards.appendChild(humidEl);
     }
+    storeCityForecast(forecastData.city.name);
 }
+
+function retrieveCityForecast(storedCity) {
+    var storageCity = localStorage.getItem(storedCity);
+    if (storageCity) {
+        storageCity = JSON.parse(storageCity);
+    }
+    return storageCity;
+}
+
 //Function to store searched city and weather results
-function storeCityForecast() {
-
+//Create a variable named storedCity when declaring the function, then use that variable as the stored key (key name); key name is whatever is inside the variable
+//The value stored for the key is the stringified version of whatever is inside the variable
+function storeCityForecast(storedCity) {
+    var retrieveStoredCity = retrieveCityForecast(storedCity);
+    if (retrieveStoredCity) {
+        console.log(retrieveStoredCity);
+    } else {
+        localStorage.setItem(storedCity, JSON.stringify(storedCity));
+        var cityButton = document.createElement('a');
+        //Create a button, and the name of the button becomes the value of the variable
+        cityButton.textContent = storedCity;
+        cityButton.classList.add('btn', 'btn-dark');
+        cityButtons.appendChild(cityButton);
+        cityButtons.addEventListener('click', storedForecastHandler);
+    }
 }
-//Function to retrieve local storage data and display it when the button for that city is clicked
-function retrieveCityForecast() {
 
-}
 //Function for event handler (button for each stored city)
 function storedForecastHandler(event) {
+    console.log(event);
+    event.preventDefault();
 
+    var retrieveCity = event.path[0].innerText;
+    cityApi(retrieveCity);
 }
+
+function retrieveAllStored() {
+    var numberOfCities = localStorage.length;
+    console.log(numberOfCities);
+
+    for (var i = 0; i < numberOfCities; i++) {
+        var storedCity = localStorage.getItem(localStorage.key(i));
+        storedCity = JSON.parse(storedCity);
+
+        var cityButton = document.createElement('a');
+        //Create a button, and the name of the button becomes the value of the variable
+        cityButton.textContent = storedCity;
+        cityButton.classList.add('btn', 'btn-dark');
+        cityButtons.appendChild(cityButton);
+        cityButtons.addEventListener('click', storedForecastHandler);
+    }
+}
+
 //Calls for the functions above
 searchFormEl.addEventListener('submit', formSubmitHandler);
+
+retrieveAllStored();
